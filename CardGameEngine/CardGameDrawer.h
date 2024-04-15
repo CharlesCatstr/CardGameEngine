3c8de27544288da9631bda7a2c0b061c7f93a093
Catstr34 <153502137+Catstr34@users.noreply.github.com>
2024-04-15 01:08:51 +0100

    Added key for J, Q & K cards
    To make it easier for users to work out the values of cards, there is now a key on the left of the game screen that shows the numerical values of Jack, Queen and King cards

--- CardGameEngine/CardGameDrawer.h
+++ CardGameEngine/CardGameDrawer.h
@@ -48,14 +48,15 @@ class CardGameDrawer
          #
  CARDS   #
  LEFT    #
+ ------- #
+ JACK:   #
+ 11      #
          #
+ QUEEN:  #
+ 12      #
          #
-         #
-         #
-         #
-         #
-         #
-         #
+ KING:   #
+ 13      #
 )";
 
 	int TemplateLineCount = -1; // Gets recalculated on construct
